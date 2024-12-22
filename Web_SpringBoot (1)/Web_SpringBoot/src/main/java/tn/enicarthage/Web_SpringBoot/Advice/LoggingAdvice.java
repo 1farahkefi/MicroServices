41dd94551e8f94fa8e56b841d61975b43dbfbb19
farahkefi <farah.kefi@enicar.ucar.tn>
2024-12-22 14:21:53 +0100

    first commit

---
+++ Web_SpringBoot (1)/Web_SpringBoot/src/main/java/tn/enicarthage/Web_SpringBoot/Advice/LoggingAdvice.java
@@ -0,0 +1,30 @@
+package LoggingAdvice.java;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAdvice {
+    Logger log= LoggerFactory.getLogger(LoggingAdvice.class);
+
+    @Pointcut(value = "")
+    public  void myPointcut(){}
+    public Object applicationLogger(ProceedingJoinPoint pjp) throws Throwable
+    {ObjectMapper mapper=new ObjectMapper();
+     String methodName=pjp.getSignature().getName();
+     String className=pjp.getTarget().getClass().toString();
+     Object[] array= pjp.getArgs();
+     log.info("methode invoked "+ className +" : "+methodName+"()"+"arguments : "+ mapper.writeValueAsString(array));
+     Object object=pjp.proceed();
+     log.info( className +" : "+methodName+"()"+"Response : "+ mapper.writeValueAsString(object));
+
+        return  object;
+    }
+}
